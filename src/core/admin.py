bea9322f9ee307ad08c319dbc658598164aa9595
Gabriel <gabriel.svasc99@gmail.com>
2022-11-02 12:01:06 -0300

    feat(core-admin): definindo a tela admin

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin  # noqa
+"""
+Django Admin cutomizado
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Definição da página admin pro usuário"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
