143a28e8ae1ee4412b210bbbc3ccd0d704c508da
Gabriel <gabriel.svasc99@gmail.com>
2022-12-24 01:22:43 -0300

    fix(app/urls): correção no static

--- src/app/urls.py
+++ src/app/urls.py
@@ -19,6 +19,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
@@ -26,10 +27,7 @@ urlpatterns = [
         url_name='api-schema'), name='api-docs'),
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+] + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
