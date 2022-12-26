8b34dfb97d8c201d2bb07fb2e05b76b4b33c7f58
Gabriel <gabriel.svasc99@gmail.com>
2022-12-26 01:05:50 -0300

    build(django-deploy): implementando a estrutura usada para deploy

--- src/app/settings.py
+++ src/app/settings.py
@@ -21,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8(48_-%h#vlt!(c_8@p!(q(+jt1zespc$y5i-uni6%b^q$j6&a'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
